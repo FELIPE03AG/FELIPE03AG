64a135255eccb1b68d438abd4148e5a8e593abfb
Felipe <felipe.anaya6515@alumnos.udg.mx>
2024-08-03 17:38:06 -0600

    probando despues de tiempo

--- ap/config.php
+++ ap/config.php
@@ -4,7 +4,7 @@ ob_start();
 //$db_username = "root";
 //$db_password = "anayag";
 //$db_name = "granja";
-$conexion=new mysqli("localhost","root","JulioPP","granja");
+$conexion=new mysqli("localhost","root","Anayag","granja");
 
 //$link = mysqli_connect($host,$db_username,$db_password,$db_name) or
 	//die ("Ha fallado la conexion".mysqli_connect_error());
