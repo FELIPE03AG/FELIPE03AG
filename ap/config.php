7fe401fcb8dddac8e34bd42ee07c99b4132af7c6
FELIPE03AG <felipe.anaya6515@alumnos.udg.mx>
2024-08-03 18:26:43 -0600

    siono

--- ap/config.php
+++ ap/config.php
@@ -4,7 +4,7 @@ ob_start();
 //$db_username = "root";
 //$db_password = "anayag";
 //$db_name = "granja";
-$conexion=new mysqli("localhost","root","Anayag","granja");
+$conexion=new mysqli("localhost","root","Anaya","granja");
 
 //$link = mysqli_connect($host,$db_username,$db_password,$db_name) or
 	//die ("Ha fallado la conexion".mysqli_connect_error());
